902f8005702bf0fb80bd006458f4066492a3d03b
mrmissx <hi@mrmiss.my.id>
2022-11-10 11:48:19 +0700

    fix(genString): convert api id to number
    * missing semicolon causing error

--- genString.js
+++ genString.js
@@ -1,14 +1,21 @@
 const { TelegramClient } = require("telegram")
 const { StringSession } = require("telegram/sessions")
-const input = require("input")
+const input = require("input");
 
 
 (async () => {
     const stringSession = new StringSession("")
-    const apiId = await input.text("Enter your API ID: ")
+    let apiId = await input.text("Enter your API ID: ")
+    try {
+        apiId = parseInt(apiId)
+    } catch (_) {
+        console.log("Invalid API ID. Must be an integer.")
+        process.exit(1)
+    }
     const apiHash = await input.text("Enter your API hash: ")
     const client = new TelegramClient(stringSession, apiId, apiHash, {
         connectionRetries: 5,
+        deviceModel: "UserGram",
     })
     await client.start({
         phoneNumber: async () => await input.text("Please enter your number: "),
@@ -16,9 +23,9 @@ const input = require("input")
         phoneCode: async () => await input.text("Please enter the code you received: "),
         onError: (err) => console.log(err),
     })
+    await client.sendMessage("me", { message: client.session.save() })
     console.log(
         "You should now be connected and the string session is sent to your saved messages. Please check it!"
     )
-    await client.sendMessage("me", { message: client.session.save() })
     console.log("Press CTRL+C to stop the script.")
 })()
